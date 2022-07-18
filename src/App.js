581c34b789c591aedd771ba22ab3b9059095c83d
Artan Merko <97398977+artanmerko@users.noreply.github.com>
2022-07-18 15:41:48 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,32 +5,42 @@ import './App.css';
 // import Message from "./components/Message";
 // import Hello from "./components/Hello";
 // import Counter from "./components/Counter";
-//import FunctionClick from "./components/FunctionClick";
-//import ClassClick from "./components/ClassClick";
+// import FunctionClick from "./components/FunctionClick";
+// import ClassClick from "./components/ClassClick";
 // import EventBind from "./components/EventBind";
 // import ParentComponent from "./components/ParentComponent";
-import UserGreeting from "./components/UserGreeting";
+// import UserGreeting from "./components/UserGreeting";
+// import NameList from "./components/NameList";
+// import Stylesheet from "./components/Stylesheet";
+// import Inline from "./components/Inline";
+// import "./appStyles.css";
+// import styles from "./appStyles.module.css";
 
 class App extends Component{
   render() {
     return (
       <div className="App">
-      {/* <Greet name="Bruce" heroName="Batman"> */}
-          {/* <p>This is children props</p></Greet> */}
-         {/* <Greet name="Clark" heroName="Superman"> */}
-           {/* <button>Action</button></Greet>*/}
+        {/* <h1 className="error">Error</h1> */}
+        {/* <h1 className={styles.success}>Success</h1> */}
+        {/* <Greet name="Bruce" heroName="Batman"> */}
+        {/* <p>This is children props</p></Greet> */}
+        {/* <Greet name="Clark" heroName="Superman"> */}
+        {/* <button>Action</button></Greet>*/}
         {/* <Greet name="Diana" heroName="Wonder Women"/> */}
         {/* <Welcome name="Bruce" heroName="Batman"/> */}
         {/* <Welcome name="Clark" heroName="Superman"/> */}
-       {/* <Welcome name="Diana" heroName="Wonder Women"/> */}
-         {/* <Hello /> */}
+        {/* <Welcome name="Diana" heroName="Wonder Women"/> */}
+        {/* <Hello /> */}
         {/* <Message /> */}
-         {/* <Counter />  */}
-         {/* <FunctionClick /> */}
-         {/* <ClassClick /> */}
-         {/* <EventBind /> */}
-         {/* <ParentComponent /> */}
-         <UserGreeting />
+        {/* <Counter />  */}
+        {/* <FunctionClick /> */}
+        {/* <ClassClick /> */}
+        {/* <EventBind /> */}
+        {/* <ParentComponent /> */}
+        {/* <UserGreeting /> */}
+        {/* <NameList /> */}
+        {/* <Stylesheet primary={true}/> */}
+        {/* <Inline /> */}
       </div>
     );
   }
