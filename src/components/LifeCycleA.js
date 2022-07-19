5ed59b9676248e902137bb00eba422866d8c9fc0
Artan Merko <97398977+artanmerko@users.noreply.github.com>
2022-07-19 12:08:03 -0400

    Update LifeCycleA.js

--- src/components/LifeCycleA.js
+++ src/components/LifeCycleA.js
@@ -19,11 +19,32 @@ import LifeCycleB from './LifeCycleB'
     console.log('LifeCycleA ComponentDidMount')
   }
 
+  shouldComponentUpdate(){
+    console.log('LifeCycleA shouldComponetnUpdate')
+    return true
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState){
+    console.log('LifeCycleA getSnapshotBeforeUpdate')
+    return null
+  }
+
+  componentDidUpdate(){
+    console.log('LifeCycleA componentDidUpdate')
+  }
+
+  changeState = () =>{
+    this.setState({
+      name: 'CodeCademy'
+    })
+  }
+
   render() {
     console.log('LifeCycleA render')
     return (
     <div>
     <div>LifeCycleA</div>
+    <button onClick={this.changeState}>Chnage State</button>
     <LifeCycleB />
     </div>
     )
